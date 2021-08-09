074e9ce9708dc87a9aab41894c519c2322e6e9d2
Chris Houston <chrisbcoding247@gmail.com>
2021-08-09 17:02:42 -0400

    memory-game init commit

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,27 @@
+// Step 1 Import React from react
+import React from 'react'
+import logo from "./logo-wrench.png"
+
+// Step 2 Code for Component
+class MemoryCard extends React.Component{
+    constructor (props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="MemoryCard">
+                <img src={logo} alt=""></img>
+            </div>
+        )
+    }
+}
+
+
+
+// Export component for use in app.js
+export default MemoryCard
