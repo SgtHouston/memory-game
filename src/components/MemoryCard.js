93a7d67ba31e139bc566afe9e69a6198ecabcf31
Chris Houston <chrisbcoding247@gmail.com>
2021-08-10 11:09:18 -0400

    card flipping enabled

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -4,18 +4,38 @@ import logo from "./logo-wrench.png"
 
 // Step 2 Code for Component
 class MemoryCard extends React.Component{
-    constructor (props) {
-        super(props)
+    constructor () {
+        super()
         this.state = {
-            
+            isFlipped: false
         }
+    }
+
+    clickHandler = () => {
+        this.setState ({
+            isFlipped: (!this.state.isFlipped)
+        })
     }
 
 
     render() {
+        
+        let innerClass = "MemoryCard__inner";
+
+        if (this.state.isFlipped) {
+            innerClass = "MemoryCard__inner flipped"
+        }
+        
         return (
-            <div className="MemoryCard">
-                <img className="logo" src={logo} alt=""></img>
+            <div className="MemoryCard" onClick={this.clickHandler}>
+                <div className={innerClass}>
+                    <div className="MemoryCard__back">
+                        <img className="logo" src={logo} alt=""></img>
+                    </div>
+                    <div classname="MemoryCard__front">
+                        ∆
+                    </div>
+                </div>
             </div>
         )
     }
